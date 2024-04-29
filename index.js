6dc6c7334a408de8cba0ce8250aed8fd8a649afe
Oumar Yanni <oumaryanni@gmail.com>
2024-04-29 22:08:25 +0000

    After KYC Paydunya validation - Functional test version - with redirection to shopify order page in case of success

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ import crypto from "crypto";
 
 // Importe l'adaptateur Node.js spécifique pour la Shopify API pour assurer la compatibilité
 import "@shopify/shopify-api/adapters/node";
+import { url } from "inspector";
 
 // Initialise la Shopify API en utilisant la fonction shopifyApi avec les paramètres requis
 const shopify = shopifyApi({
@@ -41,6 +42,10 @@ const shopify = shopifyApi({
   adminApiAccessToken: process.env.SHOPIFY_ACCESS_TOKEN,
   restResources,
 });
+
+/*// Utilisation d'un objet en mémoire pour stocker les commandes
+const orderCache = {};
+console.log("orderCache à l'initialisation :", orderCache);*/
 
 // Crée une instance de l'application Express
 const app = express();
@@ -56,7 +61,7 @@ var setup = new paydunya.Setup({
   privateKey: process.env.PAYDUNYA_PRIVATE_KEY,
   publicKey: process.env.PAYDUNYA_PUBLIC_KEY,
   token: process.env.PAYDUNYA_TOKEN,
-  mode: "live", // Utilisez 'live' pour le mode production
+  mode: "test", // Utilisez 'live' pour le mode production
 });
 
 // Configure les informations de la boutique pour PayDunya
@@ -70,7 +75,7 @@ function configurePayDunyaStore(newOrder) {
     logoURL: process.env.STORE_LOGO_URL,
     callbackURL: process.env.STORE_GLOBAL_CALLBACK_URL,
     cancelURL: process.env.STORE_GLOBAL_CANCEL_URL,
-    returnURL: newOrder.order_status_url, // Utilisez l'URL du statut de commande
+    returnURL: newOrder.order_status_url, // Utilisez l'URL du statut de commande pour retour vers la boutique Shopify après paiement
   });
 
   return store;
@@ -92,45 +97,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Définit le port d'écoute du serveur, en utilisant une variable d'environnement ou 8080 par défaut
 const PORT = process.env.PORT || 8080;
-
-// Route pour gérer le Webhook de création de commande
-app.post("/webhook/orders/create", async (req, res) => {
-  try {
-    const hmacReceived = req.headers["x-shopify-hmac-sha256"];
-    const body = req.rawBody;
-
-    if (!verifyWebhookHmac(body, hmacReceived)) {
-      return res.status(401).send("Échec de validation HMAC.");
-    }
-
-    const newOrder = req.body;
-    const store = configurePayDunyaStore(newOrder); // Passez newOrder à la fonction
-
-    // Création de l'instance de la facture PayDunya
-    var invoice = new paydunya.CheckoutInvoice(setup, store);
-
-    invoice.description = "Order ID: " + newOrder.id;
-
-    newOrder.line_items.forEach((item) => {
-      invoice.addItem(
-        item.name,
-        item.quantity,
-        parseFloat(item.price),
-        item.quantity * parseFloat(item.price),
-        item.title
-      );
-    });
-
-    invoice.totalAmount = parseFloat(newOrder.current_total_price);
-
-    await invoice.create();
-
-    res.status(200).send({ redirect_url: invoice.url });
-  } catch (error) {
-    console.error("Erreur lors de la création de la facture:", error);
-    res.status(500).send("Erreur lors de la création de la facture.");
-  }
-});
 
 // Fonction pour vérifier le HMAC
 function verifyWebhookHmac(data, hmacHeader) {
@@ -155,6 +121,7 @@ function generateSHA512Hash(masterKey) {
   }
 }
 
+// Fonction pour marquer une commande comme "Paid" dans la boutique shopify une fois qu'elle a été validé par l'IPN Paydunya
 async function markOrderAsPaid(session, orderId) {
   if (!orderId || typeof orderId !== "string") {
     console.error("ID de commande invalide ou manquant.");
@@ -202,40 +169,153 @@ async function markOrderAsPaid(session, orderId) {
   }
 }
 
-// Route pour recevoir les notifications de paiement de PayDunya
-app.post("/paydunya/ipn", async (req, res) => {
+// Fonction pour créer une facture paydunya
+function createInvoice(store, newOrder) {
+  /*// Configuration de la boutique PayDunya
+  const store = configurePayDunyaStore(newOrder);
+  console.log("Store dans la fonction createInvoice :", store);*/
+
+  // Création de l'instance de la facture PayDunya
+  const invoice = new paydunya.CheckoutInvoice(setup, store);
+
+  // Ajout de l'ID de la commande Shopify dans la description
+  invoice.description = `Order ID: ${newOrder.id}`;
+
+  // Ajout des articles à la facture
+  newOrder.line_items.forEach((item) => {
+    invoice.addItem(
+      item.name,
+      item.quantity,
+      parseFloat(item.price),
+      item.quantity * parseFloat(item.price),
+      item.title
+    );
+  });
+
+  // Configuration du montant total
+  invoice.totalAmount = parseFloat(newOrder.current_total_price);
+
+  /*// Crée la facture et redirige le client vers PayDunya
+  await invoice.create();*/
+
+  // Création de la facture et traitement du résultat
+  console.log("Création de la facture...");
+
+  return invoice
+    .create()
+    .then(() => {
+      console.log("Création de la facture réussie");
+      console.log("Statut de la facture :", invoice.status);
+      console.log("Token de facture :", invoice.token);
+      console.log("Texte de réponse :", invoice.responseText);
+      console.log("URL de redirection :", invoice.url);
+
+      return { token: invoice.token, url: invoice.url };
+    })
+    .catch((e) => {
+      console.error("Erreur lors de la création de la facture :", e);
+      throw new Error("Erreur lors de la création de la facture.");
+    });
+}
+
+// Fonction pour vérifier le statut du paiement avec le token de facture
+function checkPaymentStatus(store, token) {
+  /*if (!token) {
+    throw new Error("L'ID de commande est manquant ou invalide.");
+  }
+
+  const token = orderId; // Remplacer par le token du cache si nécessaire*/
+
+  const invoice = new paydunya.CheckoutInvoice(setup, store);
+
+  //await invoice.confirm(token);
+
+  // Création de la facture et traitement du résultat
+  /*let status;
+  let responseText;*/
+
+  return invoice
+    .confirm(token)
+    .then(() => {
+      console.log("Confirmation de la facture réussie");
+      console.log("Status de facture:", invoice.status);
+      console.log("Réponse du serveur Paydunya:", invoice.responseText);
+
+      return { status: invoice.status, responseText: invoice.responseText };
+    })
+    .catch((e) => {
+      console.error("Erreur lors de la confirmation de la facture:", e);
+      throw new Error("Erreur lors de la confirmation de la facture.");
+    });
+}
+
+// Route pour gérer le Webhook de création de commande
+app.post("/webhook/orders/create", async (req, res) => {
   try {
-    const status = req.body.data.status;
+    const hmacReceived = req.headers["x-shopify-hmac-sha256"];
+    const body = req.rawBody;
 
-    const description = req.body.data.invoice.description;
-    const orderIdMatch = description.match(/Order ID: (\d+)/);
-    const orderId = orderIdMatch
-      ? orderIdMatch[1]
-        ? orderIdMatch[1]
-        : null
-      : null;
-
-    if (!orderId) {
-      return res.status(400).send("L'ID de commande n'a pas pu être extrait.");
+    if (!verifyWebhookHmac(body, hmacReceived)) {
+      return res.status(401).send("Échec de validation HMAC.");
     }
 
-    const receivedHash = req.body.data.hash;
+    const newOrder = req.body;
+
+    const store = configurePayDunyaStore(newOrder); // Configuration du store
+
+    createInvoice(store, newOrder).then(({ token, url }) => {
+      return checkPaymentStatus(store, token).then(
+        ({ status, responseText }) => {
+          res.status(200).json({
+            redirect_url: url,
+            payment_status: status,
+            response_text: responseText,
+          });
+        }
+      );
+    });
+  } catch (error) {
+    console.error("Erreur lors de la création de la commande:", error);
+    res.status(500).send("Erreur lors de la création de la commande.");
+  }
+});
+
+// Route pour recevoir les notifications de paiement de PayDunya (IPN)
+app.post("/paydunya/ipn", async (req, res) => {
+  try {
+    const status = req.body.data.status; // Statut du paiement
+    const receivedHash = req.body.data.hash; // Hash pour validation
+
     const expectedHash = generateSHA512Hash(process.env.PAYDUNYA_MASTER_KEY);
 
     if (receivedHash !== expectedHash) {
       return res.status(401).send("Requête non authentique.");
     }
 
-    if (status === "completed") {
-      const order = await markOrderAsPaid(session, orderId);
+    const description = req.body.data.invoice.description;
+    const orderIdMatch = description.match(/Order ID: (\d+)/);
+    const orderId = orderIdMatch ? orderIdMatch[1] : null;
 
-      res.status(200).send(`Paiement réussi pour la commande ${orderId}.`);
+    if (!orderId) {
+      return res.status(400).send("L'ID de commande n'a pas pu être extrait.");
+    }
+
+    if (status === "completed") {
+      const success = await markOrderAsPaid(session, orderId);
+
+      if (success) {
+        res.status(200).send(`Paiement réussi pour la commande ${orderId}.`);
+      } else {
+        res
+          .status(500)
+          .send("Erreur lors de la mise à jour du statut de commande.");
+      }
     } else {
       res.status(200).send("Paiement échoué ou annulé.");
     }
   } catch (error) {
-    console.error("Erreur lors du traitement du paiement:", error);
-    res.status(500).send("Erreur lors du traitement du paiement.");
+    console.error("Erreur lors du traitement de l'IPN:", error);
+    res.status(500).send("Erreur lors du traitement de l'IPN.");
   }
 });
 
